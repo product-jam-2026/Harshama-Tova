fc754ae8ababb078a4ec833a0d59d33c420bb76d
Noam Maoz <noam.maoz1@mail.huji.ac.il>
2025-12-19 15:27:09 +0200

    Completed the login logic, with splitting into admins or users depending on the login email (by setting up DB in supabase).

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -5,10 +5,33 @@ import { NextResponse } from "next/server";
 export async function GET(request: Request) {
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
+  const origin = requestUrl.origin;
+
+  // Set default redirect path to home page
+  let next = "/";
+
   if (code) {
     const cookieStore = cookies();
     const supabase = createClient(cookieStore);
-    await supabase.auth.exchangeCodeForSession(code);
+
+    // Exchange the auth code for a user session (this logs the user in)
+    const { data: { user }, error } = await supabase.auth.exchangeCodeForSession(code);
+
+    if (!error && user?.email) {
+      // Check if the logged-in user exists in the 'admin_list' table
+      const { data: adminUser } = await supabase
+        .from('admin_list')
+        .select('email')
+        .eq('email', user.email)
+        .single();
+
+      // If user is found in the list, redirect to the admin page
+      if (adminUser) {
+        next = "/admin";
+      }
+    }
   }
-  return NextResponse.redirect(requestUrl.origin);
+
+  // Redirect the user to Participant or Admin side
+  return NextResponse.redirect(`${origin}${next}`);
 }
