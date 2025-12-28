33df4457cc65c5639621194404acaa230f032a8c
Shiri Karni <shiri@innovate.huji.ac.il>
2025-12-28 23:49:49 +0200

    began styling for onboarding and signup

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 import Onboarding from "@/components/Onboarding";
+import styles from "./page.module.css";
 
 export default function Home() {
   // Always show onboarding - it will redirect to /login
-  return <Onboarding />;
+  return (
+    <div className={styles.onboardingPage} data-hide-video-background>
+      <Onboarding />
+    </div>
+  );
 }
