9d0ef1263de90aca26b8996b48f846a144005d4a
Noam Maoz <noam.maoz1@mail.huji.ac.il>
2026-01-18 22:22:53 +0200

    used Mentor Icon svg and added in the global removing default scrollbar.

--- app/admin/components/TodaysActivities/ActivityCard.tsx
+++ app/admin/components/TodaysActivities/ActivityCard.tsx
@@ -51,11 +51,13 @@ export default function ActivityCard({
           </span>
 
           <div className={styles.mentorContainer}>
-            {/* Mentor Icon */}
-            <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 10 13" fill="none">
-              <path d="M4.91451 5.32283C6.24585 5.32283 7.32511 4.24211 7.32511 2.90897C7.32511 1.57584 6.24585 0.495117 4.91451 0.495117C3.58317 0.495117 2.50391 1.57584 2.50391 2.90897C2.50391 4.24211 3.58317 5.32283 4.91451 5.32283Z" stroke="#3A3A36" stroke-width="0.99" stroke-linecap="round"/>
-              <path d="M9.33399 11.7597V10.1505C9.33399 7.65776 7.32917 5.32275 4.91455 5.32275C2.49993 5.32275 0.495117 7.65776 0.495117 10.1505V11.7597" stroke="#3A3A36" stroke-width="0.99" stroke-linecap="round"/>
-            </svg>
+            {/* Updated to use the imported MentorIcon from public/icons */}
+            <img 
+              src="/icons/MentorIcon.svg" 
+              alt="מנחה" 
+              width="13" 
+              height="13" 
+            />
             
             <span className={`sadot ${styles.mentorName}`}>
               {mentor || ''}
