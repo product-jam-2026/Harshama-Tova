bcc21b27f923551da42c274e39cc27471907da6b
Noam Maoz <noam.maoz1@mail.huji.ac.il>
2026-01-18 13:37:50 +0200

    fixed the left arrow in the requests in the home page.

--- components/buttons/BackButton.tsx
+++ components/buttons/BackButton.tsx
@@ -8,15 +8,17 @@ interface BackButtonProps {
 }
 
 export default function BackButton({ href, direction = 'right', className = '' }: BackButtonProps) {
+  const iconSrc = direction === 'left' ? "/icons/LeftArrow.svg" : "/icons/back.svg";
+
   return (
     <Link 
       href={href} 
       className={`${styles.container} ${className}`}
     >
       <img 
-        src="/icons/back.svg" 
-        alt="Back" 
-        className={`${styles.icon} ${direction === 'left' ? styles.rotate : ''}`} 
+        src={iconSrc} 
+        alt={direction === 'left' ? "חזור" : "המשך"} 
+        className={styles.icon} 
       />
     </Link>
   );
